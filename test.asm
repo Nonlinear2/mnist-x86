78981a79f3257fd36c0d045679977e0a2ea28432
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-04-09 00:59:30 +0200

    update tests

--- test.asm
+++ test.asm
@@ -1,12 +1,92 @@
-global initialize_device_context
+global WindowProcessMessage
 
-
+extern GetModuleHandleW
 extern CreateCompatibleDC
 extern CreateDIBSection
 extern SelectObject
+extern GetStockObject
+extern RegisterClassW
+extern AdjustWindowRect
+extern CreateWindowExW
+extern LoadCursorW
+extern SetCursor
+extern PeekMessageW
+extern WindowProcessMessage
+extern DispatchMessageW
+extern InvalidateRect
+extern DefWindowProcW
+extern SetCapture
+extern BeginPaint
+extern BitBlt
+extern EndPaint
+
+extern UpdateWindow
+extern ReleaseCapture
+
+extern MakeIntResourceW
+
+extern load_weights
+extern load_digit_image
+extern clear_draw_region
+extern draw_circle_on_digits
+extern get_draw_region_features
+extern update_on_mouse_click
+extern run_network
 
 
-%define bitmap_info_offset                      28
+extern quit
+extern lmb_down
+extern draw_buffer
+extern digits_buffer
+extern mnist_array
+extern saved_digits_buffer
+extern digits_buffer_pixels
+extern dense1_weights
+extern dense1_bias
+extern dense2_weights
+extern dense2_bias
+extern output_buffer
+extern paint
+
+%define WINDOW_X                    650
+%define WINDOW_Y                    560
+%define MNIST_SIZE                  28
+%define DRAW_REGION_SIZE            WINDOW_Y
+%define SCALE                       WINDOW_Y / MNIST_SIZE
+%define DIGITS_IMAGE_X              50
+%define DIGITS_IMAGE_Y              560
+%define DIGITS_IMAGE_BYTE_SIZE      DIGITS_IMAGE_X*DIGITS_IMAGE_Y*4
+
+%define DRAW_BUFFER_BYTE_SIZE       WINDOW_Y * WINDOW_Y * 4
+%define DIGITS_BUFFER_BYTE_SIZE     DIGITS_IMAGE_X * DIGITS_IMAGE_Y * 4
+%define MNIST_ARRAY_BYTE_SIZE       MNIST_SIZE * MNIST_SIZE * 4
+
+%define MNIST_SIZE                      28
+%define INPUT_SIZE                      MNIST_SIZE*MNIST_SIZE
+
+%define DENSE1_SIZE                     128
+%define DENSE2_SIZE                     10
+
+%define DENSE1_BYTE_SIZE                4*DENSE1_SIZE
+%define DENSE2_BYTE_SIZE                4*DENSE2_SIZE
+
+%define DENSE1_WEIGHTS_BYTE_SIZE        INPUT_SIZE*DENSE1_SIZE*4
+%define DENSE2_WEIGHTS_BYTE_SIZE        DENSE1_SIZE*DENSE2_SIZE*4
+
+
+%define draw_buffer.pixels                      draw_buffer + 0
+%define draw_buffer.bitmap                      draw_buffer + 8
+%define draw_buffer.frame_device_context        draw_buffer + 16
+%define draw_buffer.width                       draw_buffer + 20
+%define draw_buffer.height                      draw_buffer + 24
+%define draw_buffer.bitmap_info                 draw_buffer + 28
+
+%define digits_buffer.pixels                    digits_buffer + 0
+%define digits_buffer.bitmap                    digits_buffer + 8
+%define digits_buffer.frame_device_context      digits_buffer + 16
+%define digits_buffer.width                     digits_buffer + 20
+%define digits_buffer.height                    digits_buffer + 24
+%define digits_buffer.bitmap_info               digits_buffer + 28
 
 section .text
 WindowProcessMessage:
@@ -14,7 +94,7 @@ WindowProcessMessage:
     mov     rbp, rsp
     ; Reserve 32 bytes of shadow space + 8 bytes for local variables + 8 bytes for 16 byte alignement
     sub     rsp, 48                               
-    
+
     ; use shadow space, 
     %define window_handle                           rbp + 2*8           ; rcx home
     %define message                                 rbp + 3*8           ; rdx home
@@ -59,7 +139,7 @@ WindowProcessMessage:
     ; mov r8, [wParam]
     ; mov r9, [lParam]
     call DefWindowProcW
-    jmp .break
+    jmp .return
 
     .destroy:
     mov BYTE [rel quit], 1
@@ -126,9 +206,9 @@ WindowProcessMessage:
     lea r8, [rel dense1_bias]
     lea r9, [rel dense2_weights]
     lea rax, [rel dense2_bias]
-    mov QWORD [rsp + 5 * 8], rax
+    mov QWORD [rsp + 4 * 8], rax
     lea rax, [rel output_buffer]
-    mov QWORD [rsp + 6 * 8], rax
+    mov QWORD [rsp + 5 * 8], rax
     call run_network
     add rsp, 16                                ; clear parameter space
 
@@ -168,8 +248,7 @@ WindowProcessMessage:
     jmp .break
 
     .paint:
-
-    lea rcx, [window_handle]
+    mov rcx, [window_handle]
     lea rdx, [rel paint]
     call BeginPaint
 
@@ -178,35 +257,35 @@ WindowProcessMessage:
 
     ; call BitBlt twice
 
-    sub rsp, 5 * 8                                  ; 5 stack parameters, rsp is still 16 byte aligned
+    sub rsp, 6 * 8                                  ; 5 stack parameters + 8 padding bytes, rsp is still 16 byte aligned
     mov rcx, [device_context]
     xor rdx, rdx
     xor r8, r8
     mov r9, WINDOW_Y
 
-    mov QWORD [rsp + 5 * 8], WINDOW_Y
-    mov QWORD [rsp + 6 * 8], draw_buffer.frame_device_context
+    mov QWORD [rsp + 4 * 8], WINDOW_Y
+    mov QWORD [rsp + 5 * 8], draw_buffer.frame_device_context
+    mov QWORD [rsp + 6 * 8], 0
     mov QWORD [rsp + 7 * 8], 0
-    mov QWORD [rsp + 8 * 8], 0
-    mov QWORD [rsp + 9 * 8], 0x00CC0020             ; SRCCOPY
+    mov QWORD [rsp + 8 * 8], 0x00CC0020             ; SRCCOPY
     call BitBlt
 
     mov rcx, [device_context]
-    mov rdx, WINDOW_Y + 10
+    mov rdx, WINDOW_Y
+    add rdx, 10
     xor r8, r8
     mov r9, DIGITS_IMAGE_X
 
-    mov QWORD [rsp + 5 * 8], DIGITS_IMAGE_Y
-    mov QWORD [rsp + 6 * 8], draw_buffer.frame_device_context
+    mov QWORD [rsp + 4 * 8], DIGITS_IMAGE_Y
+    mov QWORD [rsp + 5 * 8], digits_buffer.frame_device_context
+    mov QWORD [rsp + 6 * 8], 0
     mov QWORD [rsp + 7 * 8], 0
-    mov QWORD [rsp + 8 * 8], 0
-    mov QWORD [rsp + 9 * 8], 0x00CC0020             ; SRCCOPY
+    mov QWORD [rsp + 8 * 8], 0x00CC0020             ; SRCCOPY
     call BitBlt
 
-    sub rsp, 5 * 8                                  ; clear parameter space
+    add rsp, 6 * 8                                  ; clear parameter space
 
-
-    lea rcx, [window_handle]
+    mov rcx, [window_handle]
     lea rdx, [rel paint]
     call EndPaint
     jmp .break
@@ -214,7 +293,7 @@ WindowProcessMessage:
     .break:
     ; Function epilogue
     xor rax, rax                  ; Return 0
-    
+    .return:
     mov rsp, rbp ; Deallocate local variables
     pop rbp ; Restore the caller's base pointer value
     ret
